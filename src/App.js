a2ddba1be5cfffb715cfd235567b5e799694885b
Shiran Naor <shiranaor@gmail.com>
2020-01-07 15:05:59 +0200

    created empty react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="title">Genie</div>
+    </div>
+  );
+}
+
+export default App;
